20e1a497441f3471a5e2485627716c8c846e426f
Jakub Piecuch <jakub.piecuch@jakubpiecuch.pl>
2018-08-19 21:38:19 +0200

    Update security

--- src/main/java/com/jakubpiecuch/web/SecurityConfig.java
+++ src/main/java/com/jakubpiecuch/web/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.jakubpiecuch.web;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2SsoDefaultConfiguration;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 @EnableOAuth2Sso
+@EnableZuulProxy
 public class SecurityConfig extends OAuth2SsoDefaultConfiguration {
 
     public SecurityConfig(ApplicationContext applicationContext) {
