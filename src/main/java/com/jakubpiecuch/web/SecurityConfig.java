f6333945d1486bbc5e548a1ceb9580447165121d
Jakub Piecuch <jakub.piecuch@jakubpiecuch.pl>
2018-08-19 21:07:40 +0200

    Update security

---
+++ src/main/java/com/jakubpiecuch/web/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.jakubpiecuch.web;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2SsoDefaultConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableOAuth2Sso
+public class SecurityConfig extends OAuth2SsoDefaultConfiguration {
+
+    public SecurityConfig(ApplicationContext applicationContext) {
+        super(applicationContext);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable().antMatcher("/**").authorizeRequests()
+                .antMatchers("/login**").permitAll()
+                .anyRequest().authenticated();
+    }
+}
--- src/main/java/com/jakubpiecuch/web/WebApplication.java
+++ src/main/java/com/jakubpiecuch/web/WebApplication.java
@@ -2,13 +2,10 @@ package com.jakubpiecuch.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-
 @EnableZuulProxy
-@EnableOAuth2Sso
 public class WebApplication {
 
     public static void main(String[] args) {
